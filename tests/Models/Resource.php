22110f354589bf299f6bde9388bc561fd1f85e7c
guifavere <guilherme.favere@gmail.com>
2023-06-08 16:46:53 -0300

    remove trait: SimpleScopes from model: Resource

--- tests/Models/Resource.php
+++ tests/Models/Resource.php
@@ -2,10 +2,8 @@
 
 namespace GuiFavere\LaravelSimpleScopes\Tests\Models;
 
-use GuiFavere\LaravelSimpleScopes\SimpleScopes;
 use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-    use SimpleScopes;
 }
